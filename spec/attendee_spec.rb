5b262c3a74ae5f8a65674d394ee58bea860d389f
Jim Macur <jim.macur@gmail.com>
2024-07-29 09:09:51 -0600

    test: attendee exists

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -5,3 +5,15 @@ require './lib/attendee'
 RSpec.configure do |config|
     config.formatter = :documentation
 end
+
+RSpec.describe Attendee do
+    before(:each) do
+        @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    end
+
+    describe 'initialize' do
+        it 'exists' do
+            expect(@attendee).to be_an_instance_of Attendee
+        end
+    end
+end
