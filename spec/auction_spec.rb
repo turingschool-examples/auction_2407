14ce4e927fb14c2189c62f2d8b94e96ec26d1c00
Kyle Delaney <khdelaney99@gmail.com>
2024-07-29 12:15:41 -0400

    add test for #unpopular_items for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Auction do
         @auction = Auction.new
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+        @item3 = Item.new('Homemade Chocolate Chip Cookies')
+        @item4 = Item.new('2 Days Dogsitting')
+        @item5 = Item.new('Forever Stamps')
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     end
 
     describe '#initialize' do
@@ -36,6 +42,22 @@ RSpec.describe Auction do
             @auction.add_item(@item2)
 
             expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+        end
+    end
+
+    describe '#unpopular_items' do
+        it 'returns an array of items which have no bids' do
+            @auction.add_item(@item1)
+            @auction.add_item(@item2)
+            @auction.add_item(@item3)
+            @auction.add_item(@item4)
+            @auction.add_item(@item5)
+            @item1.add_bid(@attendee1, 22)
+            @item1.add_bid(@attendee2, 20)
+            @item2.add_bid(@attendee2, 20)
+            @item4.add_bid(@attendee3, 50)
+
+            expect(@auction.unpopular_items).to eq([@item3, @item5])
         end
     end
 end
