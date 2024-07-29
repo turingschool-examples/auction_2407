d1f2f57822c50f86a758186c065b313adaa20ff1
John Hill <jphill1989@gmail.com>
2024-07-29 09:37:52 -0600

    Add add_item test & method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,6 +5,8 @@ require './lib/attendee'
 RSpec.describe Auction do
     before(:each) do
         @auction = Auction.new
+        @item1 = Item.new('Chalkware Piggy Bank')
+        @item2 = Item.new('Bamboo Picture Frame')
     end
 
     describe 'Initialize' do
@@ -15,7 +17,15 @@ RSpec.describe Auction do
         it "contains an array for items, since no items, it should be empty" do
             expect(@auction.items).to eq([])
         end
+    end
 
+    describe 'add_item' do
+        it 'can add items' do
+            auction.add_item(@item1)
+            auction.add_item(@item2)
+
+            expect(@auction.items).to eq([@item1, @item2])
+        end
     end
 
 end
