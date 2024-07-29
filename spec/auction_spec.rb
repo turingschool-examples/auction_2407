ca081fa6cdbf94ec3c1242db45df333c49160677
Shane Galvin <Sgalvin36@gmail.com>
2024-07-29 09:31:56 -0600

    Add initialize class for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,1 +1,15 @@
 require './lib/auction'
+
+RSpec.describe Auction do
+    describe 'initialize' do
+        it 'exists' do
+            expect(Auction.new).to be_a Auction
+        end
+
+        it 'starts with an empty array of items' do
+            auction = Auction.new
+
+            expect(auction.items).to eq []
+        end
+    end
+end
