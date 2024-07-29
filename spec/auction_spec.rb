dc9be5178f311d14fbe3b845e000564b71bbc1cc
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 09:23:43 -0600

    test: items array

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,4 +1,6 @@
 require './lib/auction'
+require './lib/item'
+
 
 RSpec.describe Auction do
   before(:each) do
@@ -8,6 +10,12 @@ RSpec.describe Auction do
   describe '#initalize' do
     it 'exists' do
       expect(@auction).to be_an_instance_of Auction
+    end
+  end
+
+  describe '#items' do
+    it 'has items' do
+      expect(@auction.items).to eq([])
     end
   end
 end
