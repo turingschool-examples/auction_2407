721ede209f06a21ca18216ceed9d5ac146fc4e8f
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-29 12:03:00 -0500

    Feat: unpopular_items & potential_revenue

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -103,8 +103,8 @@ RSpec.describe Auction do
         end
     end
    
-    describe 'how it checks item popularity' do
-        xit 'checks current high bid' do
+    describe 'how it checks item popularity & potential revenue' do
+        it 'checks current high bid' do
             auction = Auction.new
             item1 = Item.new('Chalkware Piggy Bank')
             item2 = Item.new('Bamboo Picture Frame')
@@ -126,11 +126,36 @@ RSpec.describe Auction do
             item1.add_bid(attendee1, 22)
             item4.add_bid(attendee3, 50)
 
-            expect(auction.unpopular_items).to eq(22)
+            expect(auction.unpopular_items).to eq([item2, item3, item5])
 
             item3.add_bid(attendee2, 15)
 
-            # expect(auction.unpopular_items).to eq(22)
+            expect(auction.unpopular_items).to eq([item2, item5])
+        end
+
+        it 'checks potential revenue' do
+            auction = Auction.new
+            item1 = Item.new('Chalkware Piggy Bank')
+            item2 = Item.new('Bamboo Picture Frame')
+            item3 = Item.new('Homemade Chocolate Chip Cookies')
+            item4 = Item.new('2 Days Dogsitting')
+            item5 = Item.new('Forever Stamps')
+
+            attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+            attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+            attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+            auction.add_item(item1)
+            auction.add_item(item2)
+            auction.add_item(item3)
+            auction.add_item(item4)
+            auction.add_item(item5)
+
+            item3.add_bid(attendee2, 15)
+            item1.add_bid(attendee1, 22)
+            item4.add_bid(attendee3, 50)
+
+            expect(auction.potential_revenue).to eq(87)
         end
     end
 end
