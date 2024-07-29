360925fc58302b17d31b2196a9929345a94277e3
Jim Macur <jim.macur@gmail.com>
2024-07-29 09:07:08 -0600

    test: item has name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -16,5 +16,9 @@ RSpec.describe Item do
         it 'exists' do
             expect(@item1).to be_an_instance_of Item
         end
+
+        it 'has a name' do
+            expect(@item1.name).to eq 'Chalkware Piggy Bank'
+        end
     end
 end
