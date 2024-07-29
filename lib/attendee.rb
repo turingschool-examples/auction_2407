3e6ccc372162f406676674c3fe4f4ae4c6468f4b
Shane Galvin <Sgalvin36@gmail.com>
2024-07-29 09:22:01 -0600

    Add test for converting string to number

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,8 +1,12 @@
 class Attendee
-    attr_reader :name
+    attr_reader :name, :budget
 
     def initialize(hash)
         @name = hash[:name]
         @budget = hash[:budget]
+    end
+
+    def budget_convert(string)
+        string.
     end
 end
--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -5,5 +5,22 @@ RSpec.describe Attendee do
         it 'exists' do
             expect(Attendee.new({name: 'Megan', budget: '$50'})).to be_a Attendee
         end
+
+        it 'has attributes name and budget' do
+            attendee = Attendee.new({name: 'Megan', budget: '$50'})
+
+            expect(attendee.name).to eq 'Megan'
+            expect(attendee.budget).to eq 50
+        end
+
+        it 'modifies budget string into integer' do
+            attendee = Attendee.new({name: 'Megan', budget: '$50'})
+            start_string = '$50'
+            budget = attendee.budget_convert
+
+            expect(budget).to eq 50
+        end
+
     end
+
 end
