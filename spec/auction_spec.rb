3629cadae758c04d06470c08127aeb170e0ca613
Candice Cirbo <thecirbos@icloud.com>
2024-07-29 09:09:16 -0600

    Initial commit with starting tests

---
+++ spec/auction_spec.rb
@@ -0,0 +1,42 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.configure do |config| 
+ config.formatter = :documentation 
+end
+
+RSpec.describe Auction do 
+ before(:each) do 
+  @auction = Auction.new
+  @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+  @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+  @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+  @item1 = Item.new('Chalkware Piggy Bank')
+  @item2 = Item.new('Bamboo Picture Frame')
+  @item3 = Item.new('Homemade Chocolate Chip Cookies')
+  @item4 = Item.new('2 Days Dogsitting')
+  @item5 = Item.new('Forever Stamps')
+ end
+
+ it 'can exist' do
+  expect(@auction).to be_an(Auction)
+ end
+
+ it 'can have items in an array' do
+  expect(@auction.items).to eq([])
+ end
+
+ it 'can add items to the items array' do
+  @auction.add_item(@item1)
+  @auction.add_item(@item2)
+
+  expect(@auction.items).to eq([@item1, @item2])
+ end
+
+ it 'can show item names' do
+  @auction.add_item(@item1)
+  @auction.add_item(@item2)
+  expect(@auction.item_names).to eq(['Chalkware Piggy Bank','Bamboo Picture Frame'] )
+ end
+end
