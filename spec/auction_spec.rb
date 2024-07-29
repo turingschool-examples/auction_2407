9af1dfb5e475c07f1bc6d48cfb574165b73daf1e
Jim Macur <jim.macur@gmail.com>
2024-07-29 09:15:21 -0600

    test: auction exists

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,3 +5,15 @@ require './lib/attendee'
 RSpec.configure do |config|
     config.formatter = :documentation
 end
+
+RSpec.describe Auction do
+    before(:each) do
+        @auction = Auction.new
+    end
+
+    describe 'initialize' do
+        it 'exists' do
+            expect(@auction).to be_an_instance_of Auction
+        end
+    end
+end 
