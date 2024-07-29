264090fe34ec4834936a71c86444557dd892eb2b
Jim Macur <jim.macur@gmail.com>
2024-07-29 09:05:15 -0600

    test: item exists

---
+++ spec/item_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe Item do
+    before(:each) do
+        @item1 = Item.new('Chalkware Piggy Bank')
+        @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    describe 'initialize' do 
+        it 'exists' do
+            expect(@item1).to be Item
+        end
+    end
+end
