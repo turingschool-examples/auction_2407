faf44cfdbef7df07bcc89e028c768e22f39082af
Kyle Delaney <khdelaney99@gmail.com>
2024-07-29 11:39:24 -0400

    add test for #add_item() for auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Auction do
     before (:each) do
         @auction = Auction.new
+        @item1 = Item.new('Chalkware Piggy Bank')
+        @item2 = Item.new('Bamboo Picture Frame')
     end
 
     describe '#initialize' do
@@ -17,7 +19,15 @@ RSpec.describe Auction do
     end
 
     describe '#add_item()' do
+        it 'will add argument to items array' do
+            @auction.add_item(@item1)
+            
+            expect(@auction.items).to eq([@item1])
 
+            @auction.add_item(@item2)
+            
+            expect(@auction.items).to eq([@item1, @item2])
+        end
     end
 
     describe '#item_names' do
