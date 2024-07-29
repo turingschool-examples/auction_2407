3f8ba9bd9225e8a159dc34e542f3c9149883041c
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 09:42:28 -0600

    test: update items and attendees

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -4,9 +4,16 @@ require './lib/item'
 
 RSpec.describe Auction do
   before(:each) do
+    @auction = Auction.new
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
-    @auction = Auction.new
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
   end
 
   describe '#initalize' do
