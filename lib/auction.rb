83c436d6b19ac9be3907cca9d74c44342a9d6a8e
Shane Galvin <Sgalvin36@gmail.com>
2024-07-29 10:13:10 -0600

    Add unpopular_items method and tests

--- lib/auction.rb
+++ lib/auction.rb
@@ -10,4 +10,11 @@ class Auction
         @items << item
         @item_names << item.name
     end
+
+    def unpopular_items
+        unpop_items = @items.find_all do |item|
+            item.bids.count == 0
+        end
+        unpop_items
+    end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,5 +1,6 @@
 require './lib/auction'
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Auction do
     describe 'initialize' do
@@ -48,29 +49,41 @@ RSpec.describe Auction do
         end
     end
 
-    describe '#add_bid' do
+    describe '#unpopular_items' do
         before(:each) do
-            item1 = Item.new('Chalkware Piggy Bank')
-            item2 = Item.new('Bamboo Picture Frame')
-            item3 = Item.new('Homemade Chocolate Chip Cookies')
-            item4 = Item.new('2 Days Dogsitting')
-            item5 = Item.new('Forever Stamps')
-            attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
-            attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
-            attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
-            auction = Auction.new
-            auction.add_item(item1)
-            auction.add_item(item2)
-            auction.add_item(item3)
-            auction.add_item(item4)
-            auction.add_item(item5)
+            @item1 = Item.new('Chalkware Piggy Bank')
+            @item2 = Item.new('Bamboo Picture Frame')
+            @item3 = Item.new('Homemade Chocolate Chip Cookies')
+            @item4 = Item.new('2 Days Dogsitting')
+            @item5 = Item.new('Forever Stamps')
+            @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+            @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+            @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+            @auction = Auction.new
+            @auction.add_item(@item1)
+            @auction.add_item(@item2)
+            @auction.add_item(@item3)
+            @auction.add_item(@item4)
+            @auction.add_item(@item5)
         end
 
-        it 'adds a bid to the item hash' do
-            
+        it 'lists items with no bids on them' do
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
+            @item4.add_bid(@attendee3, 50)
+
+            expect(@auction.unpopular_items).to eq ([@item2, @item3, @item5])
         end
 
-        it 'has a bid with an attendee and amount' do
+        it 'can update as bids are placed' do
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
+            @item4.add_bid(@attendee3, 50)
+
+            expect(@auction.unpopular_items).to eq ([@item2, @item3, @item5])
+            @item3.add_bid(@attendee2, 15)
+
+            expect(@auction.unpopular_items).to eq ([@item2, @item5])
         end
     end
 end
