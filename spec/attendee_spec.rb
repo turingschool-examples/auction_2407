cf14b8acde1e871c2c318b0373b2079ea35dec67
Terra Manning <terradmanning@gmail.com>
2024-07-29 09:52:11 -0600

    feat: adds iteraction pattern tests to auction class

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -9,13 +9,16 @@ end
 RSpec.describe Attendee do
   before(:each) do
     @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   describe '#initialize' do
     it 'starts with attributes' do
       expect(@attendee).to be_an_instance_of(Attendee)
       expect(@attendee.name).to eq("Megan")
-      expect(@attendee.budget).to eq(50)
+      expect(@attendee.budget).to eq('$50')
     end
   end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -26,4 +26,72 @@ RSpec.describe Auction do
       expect(@auction.items).to eq([@item1, @item2])
       expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
+
+    it 'adds more auction items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      expect(@item1.bids).to eq {}
+    end
+
+    it 'adds bits' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq {@attendee2 => 20, @attendee1 => 22}
+    end
+
+    it 'identifies the current high bid' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.current_high_bid).to eq(22)
+    end
+
+    it 'itentifies unpopular items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      expect(@auction.unpopular_items).to eq [@item2, @item3, @item5]
+    end
+
+    it 'updates unpopular_items array' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.unpopular_items).to eq [@item2, @item5]
+    end
+
+    it 'adds potential revenue' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.potential_revenue).to eq(87)
+    end
 end
