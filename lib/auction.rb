82de54735b89c86841c27cc1e0864baab5f48373
Shawn Thompson <thompson_shawn@hotmail.com>
2024-07-29 13:03:03 -0500

    Feat: bidders and bidder_info

--- lib/auction.rb
+++ lib/auction.rb
@@ -26,5 +26,28 @@ class Auction
     def potential_revenue
         @items.sum { |item| item.current_high_bid.to_i }
     end
+
+    def bidders
+        bidder_list = []
+        @items.each do |item|
+            item.bids.each_key do |attendee|
+                unless bidder_list.include?(attendee)
+                    bidder_list << attendee
+                end
+            end
+        end
+        bidder_list.map {|attendee| attendee.name}
+    end
+
+    def bidder_info
+        info = {}
+        @items.each do |item|
+          item.bids.each do |attendee, amount|
+            info[attendee] ||= { budget: attendee.budget, items: [] }
+            info[attendee][:items] << item
+          end
+        end
+        info
+    end
  
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -158,4 +158,61 @@ RSpec.describe Auction do
             expect(auction.potential_revenue).to eq(87)
         end
     end
+
+    describe 'how it tracks bidders and their info' do
+        it 'lists bidders' do
+            auction = Auction.new
+            item1 = Item.new('Chalkware Piggy Bank')
+            item2 = Item.new('Bamboo Picture Frame')
+            item3 = Item.new('Homemade Chocolate Chip Cookies')
+            item4 = Item.new('2 Days Dogsitting')
+            item5 = Item.new('Forever Stamps')
+
+            attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+            attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+            attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+            auction.add_item(item1)
+            auction.add_item(item2)
+            auction.add_item(item3)
+            auction.add_item(item4)
+            auction.add_item(item5)
+
+            item1.add_bid(attendee2, 20)
+            item1.add_bid(attendee1, 22)
+            item4.add_bid(attendee3, 50)
+            item3.add_bid(attendee2, 15)
+
+            expect(auction.bidders).to eq(['Bob', 'Megan', 'Mike'])
+        end
+
+        it 'lists bidders info' do
+            auction = Auction.new
+            item1 = Item.new('Chalkware Piggy Bank')
+            item2 = Item.new('Bamboo Picture Frame')
+            item3 = Item.new('Homemade Chocolate Chip Cookies')
+            item4 = Item.new('2 Days Dogsitting')
+            item5 = Item.new('Forever Stamps')
+
+            attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+            attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+            attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+            auction.add_item(item1)
+            auction.add_item(item2)
+            auction.add_item(item3)
+            auction.add_item(item4)
+            auction.add_item(item5)
+
+            item1.add_bid(attendee2, 20)
+            item1.add_bid(attendee1, 22)
+            item4.add_bid(attendee3, 50)
+            item3.add_bid(attendee2, 15)
+
+            expect(auction.bidder_info).to eq({attendee2 => {budget: '$75', items: [item1, item3]}, attendee1 => {budget: '$50', items: [item1]}, attendee3 => {budget: '$100', items: [item4]}})
+        end
+    end
+    # `bidder_info` should return a hash with keys that are attendees, 
+    # and values that are a hash with that attendee's budget and an 
+    # array of items that attendee has bid on.
 end
