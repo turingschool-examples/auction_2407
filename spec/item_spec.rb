f4752da720ab376a40aa0218d216c5844a80d5d8
Shane Galvin <Sgalvin36@gmail.com>
2024-07-29 09:12:23 -0600

    Setup item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,3 +1,5 @@
+require './lib/item'
+
 RSpec.describe Item do
     describe 'initialize' do
         it 'exists' do
@@ -8,5 +10,18 @@ RSpec.describe Item do
             item1 = Item.new('Chalkware Piggy Bank')
 
             expect(item1.name).to eq 'Chalkware Piggy Bank'
+        end
+
+        it 'can take different names' do
+            item2 = Item.new('Bamboo Picture Frame')
+            
+            expect(item2.name).to eq 'Bamboo Picture Frame'
+        end
+
+        it 'starts with an empty hash for tracking bids' do
+            item1 = Item.new('Chalkware Piggy Bank')
+
+            expect(item1.bids).to be_a Hash
+        end
     end
 end
