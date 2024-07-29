861d02fe8396a69bf1b09eb936b788a37d7ee520
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-07-29 10:34:56 -0600

    Tinkered more with the attendee tests, added test methods to item spec file and the basic class structure to the item class file

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe '#attendee' do
         it 'has a name' do
             attendee = Attendee.new({name: 'Megan', budget: '$50'})
             # expect(attendee.name).to eq('Megan')
-            expect(attendee.name).to eq(name)
+            expect(attendee.name).to be_a(name)
         end
     end
 
@@ -17,7 +17,7 @@ RSpec.describe '#attendee' do
         it 'has a budget' do
             attendee = Attendee.new({name: 'Megan', budget: '$50'})
             # expect(attendee.budget).to eq('$50')
-            expect(attendee.budget).to eq(budget)
+            expect(attendee.budget).to be_a(budget)
         end
     end 
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,13 @@
 require './lib/item'
+
+RSpec.describe do
+    it 'is an item' do
+        item1 = Item.new('Chalkware Piggy Bank')
+        expect(item1.name).to eq('Chalkware Piggy Bank')
+    end
+
+    it 'has a name' do
+        item1 = Item.new('Chalkware Piggy Bank')
+        expect(item1.name).to eq('Chalkware Piggy Bank')
+    end
+end
