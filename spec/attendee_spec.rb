b65dde3a69cf7c513b406cbee33c8abfefa0d93f
Jim Macur <jim.macur@gmail.com>
2024-07-29 09:13:36 -0600

    test: has a name and budget

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -15,5 +15,13 @@ RSpec.describe Attendee do
         it 'exists' do
             expect(@attendee).to be_an_instance_of Attendee
         end
+
+        it 'has a name' do
+            expect(@attendee.name).to eq 'Megan'
+        end
+
+        it 'has a budget' do
+            expect(@attendee.budget).to eq '$50'
+        end
     end
 end
