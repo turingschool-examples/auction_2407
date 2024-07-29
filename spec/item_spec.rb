60498264fcad3c7b492a31f24732aeda5f1b572c
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 09:39:26 -0600

    test: bids attribute

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
   end
 
   describe 'initialize' do
@@ -11,8 +14,9 @@ RSpec.describe Item do
       expect(@item1).to be_an_instance_of Item 
     end
 
-    it 'has a name' do
+    it 'has attributes' do
       expect(@item1.name).to eq('Chalkware Piggy Bank')
+      expect(@item1.bids).to eq({})
     end
   end
 
