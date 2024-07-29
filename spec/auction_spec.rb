2d6438aac414cd95324109f0a5090903bbf982cf
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-29 11:32:40 -0400

    Added items to the empty array items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Auction do
       auction = Auction.new
       expect(auction.items).to eq([])
     end
+
+    it 'can add items' do 
+      auction = Auction.new
+      item_1 = Item.new('Chalkware Piggy Bank')
+      item_2 = Item.new('Bamboo Picture Frame')
+      auction.add_item(item_1)
+      auction.add_item(item_2)
+      expect(auction.items).to eq(item_1, item_2)
+    end
   end
 end
