c5eeffea50886fae5744a71061490546ee616197
Joel Davalos <jsdavalos@gmail.com>
2024-07-29 09:48:23 -0600

    inital commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Item do 
+  before(:each) do 
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  it 'exists' do
+    expect(@item1).to be_an_instance_of(Item)
+  end
+
+  it 'has a name' do
+    expect(@item1.name).to eq('Chalkware Piggy Bank')
+    expect(@item2.name).to eq('Bamboo Picture Frame')
+  end
+end
