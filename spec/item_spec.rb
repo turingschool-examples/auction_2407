ea61bef4cad263f56531497c2018a2dfc86ebea2
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 09:09:23 -0600

    test: has a name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Item do
     it 'exists' do
       expect(@item1).to be_an_instance_of Item 
     end
+
+    it 'has a name' do
+      expect(@item1.name).to eq('Chalkware Piggy Bank')
+    end
   end
 
 end
