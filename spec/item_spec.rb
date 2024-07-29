ba25226e3fb541508b2423dc0288fd8eb6e00046
Kyle Delaney <khdelaney99@gmail.com>
2024-07-29 11:07:07 -0400

    add tests for #initialize() for item

---
+++ spec/item_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Item do
+    before (:each) do
+        @item1 = Item.new('Chalkware Piggy Bank')
+        @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    describe '#initialize()' do
+        it 'is an instance of an item' do
+            expect(@item1).to be_a(Item)
+        end
+
+        it 'has a name' do
+            expect(@item1.name).to eq('Chalkware Piggy Bank')
+        end
+    end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+RSpec.configure do |config| 
+    config.formatter = :documentation 
+end
+
+require 'pry'
+require './lib/item'
