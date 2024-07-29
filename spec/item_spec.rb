f36203b6d1f75ec21d8427a56dfcd39dd9e82801
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-29 11:17:16 -0400

    Initialized item with name and an empty hash

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Item do 
+  describe '#initialize' do 
+    it 'has a name' do
+      item = Item.new('Chalkware Piggy Bank')
+      expect(item.name).to eq("Chalkware Piggy Bank")
+    end
+
+    it 'has an empty hash' do 
+      item = Item.new('Chalkware Piggy Bank')
+      expect(item.bids).to eq({})
+    end
+  end
+end
