28f31590e8b681cd92e61d7741b098ead81c7854
Candice Cirbo <thecirbos@icloud.com>
2024-07-29 10:07:44 -0600

    Adds stuff to item tests for tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,9 +8,11 @@ end
 
 RSpec.describe Item do 
  before(:each) do 
-
   @item1 = Item.new('Chalkware Piggy Bank')
   @item2 = Item.new('Bamboo Picture Frame')
+  @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+  @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+  @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
  end
 
  it 'exists' do
@@ -20,4 +22,26 @@ RSpec.describe Item do
  it 'has attributes' do
   expect(@item1.name).to eq("Chalkware Piggy Bank")
  end
+
+ it 'can have bids in a hash' do
+  expect(@item1.bids).to eq({})
+ end
+
+ it 'can put bids in hash' do
+  @item1.add_bid(@attendee2, 20)
+  @item1.add_bid(@attendee1, 22)
+  expected = {
+    @attendee2 => 20,
+    @attendee1 => 22
+  }
+  expect(@item1.bids).to eq(expected)
+ end
+
+ it 'can tell you current high bid' do
+  @item1.add_bid(@attendee2, 20)
+  @item1.add_bid(@attendee1, 22)
+  expect(@item1.current_high_bid).to eq(22)
+ end
+
+ 
 end
