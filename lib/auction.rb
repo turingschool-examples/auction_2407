c7dd1fbca695395ecc4ee843f48d48aa5c2b0ba9
Bryan Willett <btwillett2003@gmail.com>
2024-07-29 12:56:59 -0500

    Added bidders method to retrive names from nested hash.

--- lib/auction.rb
+++ lib/auction.rb
@@ -20,4 +20,8 @@ attr_reader :items
     def potential_revenue
         @items.sum {|item| item.current_high_bid.to_i}
     end
+
+    def bidders
+        @items.flat_map {|item| item.bids.keys}.uniq.map(&:name)
+    end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -87,5 +87,22 @@ RSpec.describe Auction do
             expect(@auction.potential_revenue).to eq(87)
         end
     end
+
+    describe 'bidders' do
+        it 'lists the names of the bidders for all items' do
+            @auction.add_item(@item1)
+            @auction.add_item(@item2)
+            @auction.add_item(@item3)
+            @auction.add_item(@item4)
+            @auction.add_item(@item5)
+      
+            @item1.add_bid(@attendee1, 22)
+            @item1.add_bid(@attendee2, 20)
+            @item4.add_bid(@attendee3, 50)
+            @item3.add_bid(@attendee2, 15)
+      
+            expect(@auction.bidders).to eq(['Megan', 'Bob', 'Mike'])
+        end
+    end
 end
 
