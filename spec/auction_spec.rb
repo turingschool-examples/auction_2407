4d96e4a73492e38595db9b59996a082306ed43da
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-07-29 11:46:23 -0600

    added the rest of the auction item instances and basic structures for methods mentioned in iteration 2

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,5 +1,6 @@
 require './lib/auction'
 require './lib/item'
+require '.lib/attendee'
 
 RSpec.describe do
     # it 'is an auction' do
@@ -18,5 +19,27 @@ RSpec.describe do
         expect(item1.name).to eq('Chalkware Piggy Bank')
         item2 = Item.new('Bamboo Picture Frame')
         expect(item2.name).to eq('Bamboo Picture Frame')
+        item3 = Item.new('Homemade Chocolate Chip Cookies')
+        expect(item3.name).to eq('Homemade Chocolate Chip Cookies')
+        item4 = Item.new('2 Days Dogsitting')
+        expect(item4.name).to eq('2 Days Dogsitting')
+        item5 = Item.new('Forever Stamps')
+        expect(item5.name).to eq('Forever Stamps')
+    end
+
+    it 'has attendees' do
+
+    end
+
+    it 'has bidding' do
+
+    end
+
+    it 'has unpopular items' do
+
+    end
+
+    it 'has potential revenue' do
+        
     end
 end
