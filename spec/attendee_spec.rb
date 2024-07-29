8642ef2997bfccad1670230c43e42d9cbfc700f1
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 09:19:26 -0600

    fix: budget sting to integer

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Attendee do
 
     it 'has attributes' do
       expect(@attendee.name).to eq 'Megan'
-      expect(@attendee.budget).to eq '$50'
+      expect(@attendee.budget).to eq 50
     end
   end
 end
