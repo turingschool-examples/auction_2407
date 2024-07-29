0a518b39f0aa4ce655a8f2249a3fd6a3e514fba1
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-29 11:49:52 -0400

    created test for add_bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,5 @@
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do 
   describe '#initialize' do 
@@ -10,6 +11,17 @@ RSpec.describe Item do
     it 'has an empty hash' do 
       item = Item.new('Chalkware Piggy Bank')
       expect(item.bids).to eq({})
+    end
+
+    describe 'add bids' do 
+      it 'adds bids' do 
+        item = Item.new('Chalkware Piggy Bank')
+        attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        item.add_bid(attendee1, 20)
+        item.add_bid(attendee2, 30)
+        expect(item.bids).to eq({attendee1 => 20, attendee2 => 30})
+      end
     end
   end
 end
