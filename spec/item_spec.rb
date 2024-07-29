65930691a4578713f2a973f1eab611cb60680d91
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-29 11:06:16 -0400

    test: #initialize

---
+++ spec/item_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Item do
+    before(:each) do
+        @item1 = Item.new('Chalkware Piggy Bank')
+        @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    describe '#initialize' do
+        it 'can initialize' do
+            expect(@item1).to be_an_instance_of(Item)
+        end
+
+        it 'has a name' do
+            expect(@item1.name).to eq("Chalkware Piggy Bank")
+        end
+    end
+end
