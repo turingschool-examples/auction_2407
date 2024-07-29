3a0cbf03020a7c356f61bd99b850d4d9ff8a2478
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 09:05:30 -0600

    Initial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+  end
+
+  describe 'initialize' do
+    it 'exists' do
+      
+    end
+  end
+
+end
