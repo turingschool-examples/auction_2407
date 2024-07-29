d0dbc4cd58ebc173ff1edb179dcabb4066eabc40
Candice Cirbo <thecirbos@icloud.com>
2024-07-29 11:32:25 -0600

    Adds last methods to auction I am doing

--- lib/auction.rb
+++ lib/auction.rb
@@ -23,6 +23,28 @@ class Auction
    @items.sum do |item|
     item.current_high_bid
    end
-  
+  end
+
+  def bidders
+   @items.flat_map do |item|
+    item.bids.keys.map do |attendee|
+      attendee.name
+    end
+   end
+  end
+
+  def bidder_info
+    bidders = Hash.new {}
+    @items.each do |item|
+      item.bids.each do |attendee, bid|
+        if bidders[attendee].nil?
+        bidders[attendee] = {}
+        bidders[attendee][:budget] = attendee.budget
+        bidders[attendee][:items] = []
+        end
+        bidders[attendee][:items] << item
+      end
+    end
+    bidders
   end
 end
