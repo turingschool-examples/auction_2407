5ccb293c55a4bc087217151b096939a8853a69b6
Candice Cirbo <thecirbos@icloud.com>
2024-07-29 10:05:42 -0600

    Had to change method for auction

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,18 @@
 class Item
-  attr_reader :name
+  attr_reader :name,
+              :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, bid)
+    @bids[attendee] = bid
+  end
+
+  def current_high_bid
+    return 0 if @bids == {}
+    @bids.values.max
   end
 end
