b2fd05c4c4fe8a8e2fab68928098033d71f01768
Joel Davalos <jsdavalos@gmail.com>
2024-07-29 10:45:04 -0600

    Auction can add and name items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe Auction do
   it 'exists' do 
     expect(@auction).to be_an_instance_of(Auction)
   end
+
+  it 'has no items' do
+    expect(@auction.items).to eq([])
+  end
+
+  it 'can add items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+
+    expect(@auction.items).to eq([@item1, @item2])
+  end
+
+  it 'can list item names' do 
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+
+    expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
 end
