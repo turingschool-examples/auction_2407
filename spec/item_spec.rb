94a2f492d6bcf8038da96dd11a297b4b60b74fad
Kyle Delaney <khdelaney99@gmail.com>
2024-07-29 11:58:23 -0400

    add test for #add_bid() for item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Item do
     before (:each) do
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
     end
 
     describe '#initialize()' do
@@ -17,6 +19,21 @@ RSpec.describe Item do
         
         it 'has bids hash' do
             expect(@item1.bids).to eq({})
+        end
+    end
+
+    describe '#add_bid()' do
+        it 'will add a KVP to the bids hash based on argument' do
+            @item1.add_bid(@attendee2, 20)
+            
+            expect(@item.bids).to eq({@attendee2 => 20})
+
+            @item1.add_bid(@attendee1, 22)
+
+            expect(@item.bids).to eq({
+                                        @attendee2 => 20,
+                                        @attendee1 => 22
+                                    })
         end
     end
 end
