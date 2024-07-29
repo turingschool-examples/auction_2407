c314bb638c440acedabc247bbe65c3b2c259cfb0
Kyle Delaney <khdelaney99@gmail.com>
2024-07-29 11:52:33 -0400

    pass bids in #initialize() test for item

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,8 @@
 class Item
-    attr_reader :name
+    attr_reader :name, :bids
 
-    def initialize(name)
+    def initialize(name, bid = {})
         @name = name
+        @bids = bid
     end
 end
