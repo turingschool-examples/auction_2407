062c6ae8aeb1d8ba4a945b3b3c359216870edde2
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-29 11:33:40 -0400

    feat: #items_names

--- lib/auction.rb
+++ lib/auction.rb
@@ -9,4 +9,12 @@ class Auction
     def add_item(item)
         @items << item
     end
+
+    def item_names
+        names_list = []
+        @items.find_all do |item|
+            names_list << item.name
+        end
+        names_list
+    end
 end
