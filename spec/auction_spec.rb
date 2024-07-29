8ba38866e8a05b9a2c71c2eec57b1d02ae3762d5
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-29 10:14:27 -0600

    test: return unpopular items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -74,5 +74,19 @@ RSpec.describe Auction do
 
       expect(@item1.current_high_bid).to eq 22
     end
+
+    it 'returns items with no bids' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(attendee3, 50)
+      expected = [@item2, @item3, @item5]
+
+      expect(@auction.unpopular_items).to eq(expected)
+    end
   end
 end
