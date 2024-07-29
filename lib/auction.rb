455ab37483bc3ca49f82874b201414ab33dd6ead
Candice Cirbo <thecirbos@icloud.com>
2024-07-29 10:07:07 -0600

    Adds tests and methods to auction

--- lib/auction.rb
+++ lib/auction.rb
@@ -11,6 +11,18 @@ class Auction
 
   def item_names
    @items.map {|item| item.name}
+  end
 
+  def unpopular_items
+   @items.select do |item|
+    item.bids.count == 0
+   end
+  end
+
+  def potential_revenue
+   @items.sum do |item|
+    item.current_high_bid
+   end
+  
   end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -39,4 +39,35 @@ RSpec.describe Auction do
   @auction.add_item(@item2)
   expect(@auction.item_names).to eq(['Chalkware Piggy Bank','Bamboo Picture Frame'] )
  end
+
+ it 'can show unpopular items' do
+  @auction.add_item(@item1)
+  @auction.add_item(@item2)
+  @auction.add_item(@item3)
+  @auction.add_item(@item4)
+  @auction.add_item(@item5)
+
+  @item1.add_bid(@attendee2, 20)
+  @item1.add_bid(@attendee1, 22)
+
+  @item4.add_bid(@attendee3, 50)
+
+  expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+
+  @item3.add_bid(@attendee2, 15)
+  expect(@auction.unpopular_items).to match_array([@item2, @item5])
+ end
+
+ it 'can tell you potential revenue' do
+  @auction.add_item(@item1)
+  @auction.add_item(@item2)
+  @auction.add_item(@item3)
+  @auction.add_item(@item4)
+  @auction.add_item(@item5)
+  @item1.add_bid(@attendee2, 20)
+  @item1.add_bid(@attendee1, 22)
+  @item4.add_bid(@attendee3, 50)
+  @item3.add_bid(@attendee2, 15)
+  expect(@auction.potential_revenue).to eq(87)
+ end
 end
