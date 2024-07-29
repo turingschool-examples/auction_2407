06ce3273c7754f95658500b8ec5da18374cfb074
John Hill <jphill1989@gmail.com>
2024-07-29 09:34:58 -0600

    Add Auction items test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe Auction do
             expect(@auction).to be_an_instance_of Auction
         end
 
+        it "contains an array for items, since no items, it should be empty" do
+            expect(@auction.items).to eq([])
+        end
+
     end
 
 end
