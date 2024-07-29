890ae68f05e16ff03c6dea606487296e30bd1b1e
John Hill <jphill1989@gmail.com>
2024-07-29 09:53:15 -0600

    Add add_bids test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,10 +1,16 @@
 
 require './lib/item'
+require './lib/attendee'
+
 
 RSpec.describe Item do
     before(:each) do
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     end
 
     describe 'Initialize' do
@@ -22,7 +28,16 @@ RSpec.describe Item do
         end
     end
 
+    describe 'add_bid' do
+        it "add bids" do
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
 
+            expect(@item1.bids).to eq({
+                @attendee2 => 20,
+                @attendee1 => 22
+            })
+        end
+    end
     
-
 end
