15d501534ca53aefb7c3b59006151f8a5a7838b9
Bryan Willett <btwillett2003@gmail.com>
2024-07-29 11:25:32 -0500

    Added rspec tests to check name and budget for all attendees.

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -6,22 +6,28 @@ end
 
 RSpec.describe Attendee do
     before(:each) do
-        @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
         @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
         @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     end
 
     describe 'Initialize' do
         it 'exists' do
-            expect(@attendee).to be_an_instance_of(Attendee)
+            expect(@attendee1).to be_an_instance_of(Attendee)
+            expect(@attendee2).to be_an_instance_of(Attendee)
+            expect(@attendee3).to be_an_instance_of(Attendee)
         end
 
         it 'has a name' do
-            expect(@attendee.name).to eq('Megan')
+            expect(@attendee1.name).to eq('Megan')
+            expect(@attendee2.name).to eq('Bob')
+            expect(@attendee3.name).to eq('Mike')
         end
 
         it 'has a budget' do
-            expect(@attendee.budget).to eq(50)
+            expect(@attendee1.budget).to eq(50)
+            expect(@attendee2.budget).to eq(75)
+            expect(@attendee3.budget).to eq(100)
         end
     end
 end
